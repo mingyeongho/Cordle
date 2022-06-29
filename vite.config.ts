8d9a581b6abfb5294deb49a0b76f5f7880f5e198
민경호 <1736s@naver.com>
2022-06-29 23:40:34 +0900

    modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cordle/",
+  base: "/Cordle/",
   plugins: [react()],
   server: {
     host: true,
